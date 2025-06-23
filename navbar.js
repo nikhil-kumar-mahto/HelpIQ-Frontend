48003e17a78018aab1b64d198207b354f4c4c58a
nikhil-mahto <nikhil@sparkbrains.in>
2025-06-23 17:51:28 +0530

    worked on responsiveness

--- navbar.js
+++ navbar.js
@@ -1,16 +1,6 @@
 const navbarToggle = document.getElementById('navbarToggle');
 const navbarLinks = document.getElementById('navbarLinks');
 
-navbarToggle.addEventListener('click', function () {
-    navbarLinks.classList.toggle('open');
-    navbarToggle.classList.toggle('open');
+navbarToggle.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
 });
-
-navbarLinks.querySelectorAll('a').forEach(link =>
-    link.addEventListener('click', () => {
-        if (window.innerWidth <= 768) {
-            navbarLinks.classList.remove('open');
-            navbarToggle.classList.remove('open');
-        }
-    })
-);
